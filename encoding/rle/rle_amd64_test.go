4a9a0f1c2360d2c41c12ee4547db48b9ae4b7e6a
Achille Roussel <achille.roussel@gmail.com>
2025-01-17 11:59:33 -0800

    fix issue 199
    Signed-off-by: Achille Roussel <achille.roussel@gmail.com>

--- encoding/rle/rle_amd64_test.go
+++ encoding/rle/rle_amd64_test.go
@@ -3,20 +3,34 @@
 
 package rle
 
-import "testing"
+import (
+	"testing"
+
+	"golang.org/x/sys/cpu"
+)
+
+func requireAVX2(tb testing.TB) {
+	if !cpu.X86.HasAVX2 {
+		tb.Skip("AVX2 not supported")
+	}
+}
 
 func TestEncodeInt32IndexEqual8ContiguousAVX2(t *testing.T) {
+	requireAVX2(t)
 	testEncodeInt32IndexEqual8Contiguous(t, encodeInt32IndexEqual8ContiguousAVX2)
 }
 
 func TestEncodeInt32IndexEqual8ContiguousSSE(t *testing.T) {
+	requireAVX2(t)
 	testEncodeInt32IndexEqual8Contiguous(t, encodeInt32IndexEqual8ContiguousSSE)
 }
 
 func BenchmarkEncodeInt32IndexEqual8ContiguousAVX2(b *testing.B) {
+	requireAVX2(b)
 	benchmarkEncodeInt32IndexEqual8Contiguous(b, encodeInt32IndexEqual8ContiguousAVX2)
 }
 
 func BenchmarkEncodeInt32IndexEqual8ContiguousSSE(b *testing.B) {
+	requireAVX2(b)
 	benchmarkEncodeInt32IndexEqual8Contiguous(b, encodeInt32IndexEqual8ContiguousSSE)
 }
