4d0e27bdca76c6ed24b1060c21148def56250c8c
Achille Roussel <achille.roussel@gmail.com>
2025-11-24 01:43:58 -0800

    combine json.Number and json.RawMessage code paths
    Signed-off-by: Achille Roussel <achille.roussel@gmail.com>

--- column_buffer_json.go
+++ column_buffer_json.go
@@ -10,97 +10,15 @@ import (
 )
 
 func writeJSONToLeaf(col ColumnBuffer, levels columnLevels, val *jsonValue, node Node) {
-	switch typ := node.Type(); val.kind() {
+	switch val.kind() {
 	case jsonNull:
 		col.writeNull(levels)
-
 	case jsonTrue, jsonFalse:
-		if kind := typ.Kind(); kind != Boolean {
-			panic(fmt.Errorf("cannot write JSON boolean to column with type %v", kind))
-		}
 		col.writeBoolean(levels, val.kind() == jsonTrue)
-
 	case jsonNumber:
-		if logicalType := typ.LogicalType(); logicalType != nil {
-			switch {
-			case logicalType.Timestamp != nil:
-				// Interpret number as seconds since Unix epoch (with sub-second precision)
-				num := val.float()
-				sec, frac := math.Modf(num)
-				t := time.Unix(int64(sec), int64(frac*1e9)).UTC()
-				writeTime(col, levels, t, node)
-				return
-
-			case logicalType.Date != nil:
-				// Interpret number as seconds since Unix epoch
-				t := time.Unix(val.int(), 0).UTC()
-				writeTime(col, levels, t, node)
-				return
-
-			case logicalType.Time != nil:
-				// Interpret number as seconds since midnight
-				num := val.float()
-				d := time.Duration(num * float64(time.Second))
-				writeDuration(col, levels, d, node)
-				return
-			}
-		}
-
-		switch kind := typ.Kind(); kind {
-		case Boolean:
-			col.writeBoolean(levels, val.float() != 0)
-		case Int32, Int64:
-			switch val.number() {
-			case jsonNumberTypeInt:
-				col.writeInt64(levels, val.int())
-			case jsonNumberTypeUint:
-				col.writeInt64(levels, int64(val.uint()))
-			case jsonNumberTypeFloat:
-				col.writeDouble(levels, val.float())
-			}
-		case Float, Double:
-			col.writeDouble(levels, val.float())
-		default:
-			col.writeByteArray(levels, unsafeByteArrayFromString(val.string()))
-		}
-
+		writeJSONNumber(col, levels, json.Number(val.string()), node)
 	case jsonString:
-		str := val.string()
-
-		if logicalType := typ.LogicalType(); logicalType != nil {
-			switch {
-			case logicalType.Timestamp != nil:
-				t, err := time.Parse(time.RFC3339, str)
-				if err != nil {
-					panic(fmt.Errorf("cannot parse JSON string %q as timestamp: %w", str, err))
-				}
-				writeTime(col, levels, t, node)
-				return
-
-			case logicalType.Date != nil:
-				t, err := time.Parse("2006-01-02", str)
-				if err != nil {
-					panic(fmt.Errorf("cannot parse JSON string %q as date: %w", str, err))
-				}
-				writeTime(col, levels, t, node)
-				return
-
-			case logicalType.Time != nil:
-				t, err := time.Parse("15:04:05.000000000", str)
-				if err != nil {
-					panic(fmt.Errorf("cannot parse JSON string %q as time: %w", str, err))
-				}
-				d := time.Duration(t.Hour())*time.Hour +
-					time.Duration(t.Minute())*time.Minute +
-					time.Duration(t.Second())*time.Second +
-					time.Duration(t.Nanosecond())*time.Nanosecond
-				writeDuration(col, levels, d, node)
-				return
-			}
-		}
-
-		col.writeByteArray(levels, unsafeByteArrayFromString(str))
-
+		writeJSONString(col, levels, val.string(), node)
 	default:
 		// Nested objects/arrays shouldn't appear in leaf nodes, but if they do,
 		// serialize back to JSON bytes for BYTE_ARRAY columns
@@ -156,32 +74,90 @@ func writeJSONToRepeated(columns []ColumnBuffer, levels columnLevels, val *jsonV
 	elementWriter(columns, levels, reflect.ValueOf(val))
 }
 
+func writeJSONString(col ColumnBuffer, levels columnLevels, str string, node Node) {
+	typ := node.Type()
+
+	if logicalType := typ.LogicalType(); logicalType != nil {
+		switch {
+		case logicalType.Timestamp != nil:
+			t, err := time.Parse(time.RFC3339, str)
+			if err != nil {
+				panic(fmt.Errorf("cannot parse JSON string %q as timestamp: %w", str, err))
+			}
+			writeTime(col, levels, t, node)
+			return
+
+		case logicalType.Date != nil:
+			t, err := time.Parse("2006-01-02", str)
+			if err != nil {
+				panic(fmt.Errorf("cannot parse JSON string %q as date: %w", str, err))
+			}
+			writeTime(col, levels, t, node)
+			return
+
+		case logicalType.Time != nil:
+			t, err := time.Parse("15:04:05.000000000", str)
+			if err != nil {
+				panic(fmt.Errorf("cannot parse JSON string %q as time: %w", str, err))
+			}
+			d := time.Duration(t.Hour())*time.Hour +
+				time.Duration(t.Minute())*time.Minute +
+				time.Duration(t.Second())*time.Second +
+				time.Duration(t.Nanosecond())*time.Nanosecond
+			writeDuration(col, levels, d, node)
+			return
+		}
+	}
+
+	col.writeByteArray(levels, unsafeByteArrayFromString(str))
+}
+
 func writeJSONNumber(col ColumnBuffer, levels columnLevels, num json.Number, node Node) {
 	typ := node.Type()
 	str := num.String()
 
-	switch kind := typ.Kind(); kind {
-	case Boolean:
-		switch jsonNumberTypeOf(str) {
-		case jsonNumberTypeInt:
-			i, err := num.Int64()
-			if err != nil {
-				panic(fmt.Errorf("cannot convert json.Number %q to boolean: %w", num, err))
-			}
-			col.writeBoolean(levels, i != 0)
-		case jsonNumberTypeUint:
-			u, err := strconv.ParseUint(str, 10, 64)
-			if err != nil {
-				panic(fmt.Errorf("cannot convert json.Number %q to boolean: %w", num, err))
-			}
-			col.writeBoolean(levels, u != 0)
-		case jsonNumberTypeFloat:
+	if logicalType := typ.LogicalType(); logicalType != nil {
+		switch {
+		case logicalType.Timestamp != nil:
+			// Interpret number as seconds since Unix epoch (with sub-second precision)
 			f, err := num.Float64()
 			if err != nil {
-				panic(fmt.Errorf("cannot convert json.Number %q to boolean: %w", num, err))
+				panic(fmt.Errorf("cannot convert json.Number %q to float64 for timestamp: %w", num, err))
 			}
-			col.writeBoolean(levels, f != 0)
+			sec, frac := math.Modf(f)
+			t := time.Unix(int64(sec), int64(frac*1e9)).UTC()
+			writeTime(col, levels, t, node)
+			return
+
+		case logicalType.Date != nil:
+			// Interpret number as seconds since Unix epoch
+			i, err := num.Int64()
+			if err != nil {
+				panic(fmt.Errorf("cannot convert json.Number %q to int64 for date: %w", num, err))
+			}
+			t := time.Unix(i, 0).UTC()
+			writeTime(col, levels, t, node)
+			return
+
+		case logicalType.Time != nil:
+			// Interpret number as seconds since midnight
+			f, err := num.Float64()
+			if err != nil {
+				panic(fmt.Errorf("cannot convert json.Number %q to float64 for time: %w", num, err))
+			}
+			d := time.Duration(f * float64(time.Second))
+			writeDuration(col, levels, d, node)
+			return
 		}
+	}
+
+	switch kind := typ.Kind(); kind {
+	case Boolean:
+		f, err := num.Float64()
+		if err != nil {
+			panic(fmt.Errorf("cannot convert json.Number %q to float64 for boolean: %w", num, err))
+		}
+		col.writeBoolean(levels, f != 0)
 
 	case Int32, Int64:
 		switch jsonNumberTypeOf(str) {
